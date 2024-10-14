f5158f528fafd4e7f8da3fbc652d084e74e00a9a
IrwinPrado <166900797+IrwinPrado@users.noreply.github.com>
2024-10-14 15:51:09 -0700

    Avance,Auth,Login,Restricciones

---
+++ demo.jwt/src/main/java/irojas/demo/jwt/Auth/AuthController.java
@@ -0,0 +1,26 @@
+package irojas.demo.jwt.Auth;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+
+public class AuthController {
+
+    @PostMapping(value="login")
+    public String login(){  
+
+        return "Login from public endpoint";
+    }
+
+    @PostMapping(value="register")
+    public String register(){  
+
+        return "REgister from public endpoint";
+    }
+}
---
+++ demo.jwt/src/main/java/irojas/demo/jwt/Demo/DemoController.java
@@ -0,0 +1,15 @@
+package irojas.demo.jwt.Demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
+public class DemoController {
+    public String welcome(){
+        return "Welcome form secure endpoint";
+    }
+}
